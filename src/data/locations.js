e550c3c5c3e5e8f6ce732dd3fba8e4150c44eb1c
pradnya <pradnyahegde@gmail.com>
2020-05-22 15:41:16 -0400

    initial list of locations

---
+++ src/data/locations.js
@@ -0,0 +1,24 @@
+export const locations = [
+    {
+      placename: "Herndon, VA",
+      date: "August 1, 2015",
+      location: {
+        lat: 38.958988,
+        lng: -77.417320
+      },
+      todo: [
+        "Where we start! 🚀"
+      ]
+    },
+    {
+      placename: "Middlesboro, KY",
+      date: "August 1, 2015",
+      location: {
+        lat: 36.627517,
+        lng: -83.621635
+      },
+      todo: [
+        "Cumberland Gap 🌳"
+      ]
+    }
+  ];
